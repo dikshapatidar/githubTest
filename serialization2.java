16a0fca8b480df917fbcbea1bdb76414be2eb9a9
dikshapatidar <diksha95patidar@gmail.com>
2021-05-05 04:09:18 -0700

    Initial commit

---
+++ serialization2.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+	 transient  final int i = 10;
+  transient static int j = 20;
+
+}
+
+class SerializeDemo {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		Dog d1 = new Dog();
+
+		FileOutputStream fos = new FileOutputStream("abc.ser");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(d1);
+
+		FileInputStream fis = new FileInputStream("abc.ser");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Dog d2 = (Dog) ois.readObject();
+
+		System.out.println(d2.i + "......." + d2.j);
+
+	}
+}
